7dd4fb03716e3a3b1282b32d7286ae24f036dc86
Krystian Igras <krystian8207@gmail.com>
2018-10-27 12:57:56 +0200

    Add name param for add_validations

--- example/validator_example.R
+++ example/validator_example.R
@@ -7,15 +7,25 @@ validator <- Validator$new()
 sample_data <- tibble(
   x = letters[1:3],
   y = letters[1:3],
-  z = 1:3) %>%
-  clear_results() %>%  chain_start() %>%
-  verify(title = "x should have character class", v_class(x) == "character") %>%
-  verify(title = "y should have numeric class", v_class(y) == "numeric") %>%
-  verify(title = "y should have date class", v_class(y) == "Date") %>%
-  verify(title = "z should have Date class", ignore_chain_funs = TRUE,
-         error_fun = append_as_warning, v_class(z) == "Date") %>%
-  chain_end(error_fun = error_append)
+  z = 1:3)
 
-validator$add_validations(sample_data)
+validation_rules <- function(data) {
+  data %>%
+    clear_results() %>%  chain_start() %>%
+    verify(title = "x should have character class", v_class(x) == "character") %>%
+    verify(title = "y should have numeric class", v_class(y) == "numeric") %>%
+    verify(title = "y should have date class", v_class(y) == "Date") %>%
+    verify(title = "z should have Date class", ignore_chain_funs = TRUE,
+           error_fun = append_as_warning, v_class(z) == "Date") %>%
+    chain_end(error_fun = error_append)
+}
+
+sample_data %>%
+  validation_rules() %>%
+  validator$add_validations(., "sample_data")
+
+sample_data %>%
+  validation_rules() %>%
+  validator$add_validations(.)
 
 validator$get_validations()
