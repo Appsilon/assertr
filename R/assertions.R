fad80b5bdbae63e8527a2ab8c8f46778f47e4ccc
rabiibouhestine <rabiibouhestine@outlook.com>
2021-01-03 15:14:55 +0100

    removed explicit call to assertr functions

--- R/assertions.R
+++ R/assertions.R
@@ -8,44 +8,44 @@ validate <- function(data, name, description = NULL) {
   }
   attr(data, "data-description") <- description
   attr(data, "data-name") <- name
-  attr(data, "assertr_in_chain_success_fun_override") <- assertr::success_append
-  attr(data, "assertr_in_chain_error_fun_override") <- assertr::error_append
+  attr(data, "assertr_in_chain_success_fun_override") <- success_append
+  attr(data, "assertr_in_chain_error_fun_override") <- error_append
   data
 }
 
 #' @export
 assert_if <- function(data, expr, description = NA, obligatory = FALSE) {
-  assertr::verify(data = data,
+  verify(data = data,
                   expr = !!rlang::enexpr(expr),
                   description = format_description(description),
                   skip_chain_opts = FALSE,
                   obligatory = obligatory,
-                  success_fun = assertr::success_append,
-                  error_fun = assertr::error_append,
-                  defect_fun = assertr::defect_append)
+                  success_fun = success_append,
+                  error_fun = error_append,
+                  defect_fun = defect_append)
 }
 
 #' @export
 assert_cols <- function(data, predicate, ..., predicate_calculated_from_column = FALSE, obligatory = FALSE, description = NA) {
   if (predicate_calculated_from_column) {
-    data <- assertr::insist(
+    data <- insist(
       data = data, predicate_generator = predicate, ...,
       skip_chain_opts = FALSE,
       obligatory = obligatory,
       description = format_description(description),
-      success_fun = assertr::success_append,
-      error_fun = assertr::error_append,
-      defect_fun = assertr::defect_append
+      success_fun = success_append,
+      error_fun = error_append,
+      defect_fun = defect_append
     )
   } else {
-    data <- assertr::assert(
+    data <- assert(
       data = data, predicate = predicate, ...,
       skip_chain_opts = FALSE,
       obligatory = obligatory,
       description = format_description(description),
-      success_fun = assertr::success_append,
-      error_fun = assertr::error_append,
-      defect_fun = assertr::defect_append
+      success_fun = success_append,
+      error_fun = error_append,
+      defect_fun = defect_append
     )
   }
 }
@@ -53,7 +53,7 @@ assert_cols <- function(data, predicate, ..., predicate_calculated_from_column =
 #' @export
 assert_rows <- function(data, row_reduction_fn, predicate, ..., predicate_calculated_from_reduced_row = FALSE, obligatory = FALSE, description = NA) {
   if (predicate_calculated_from_reduced_row) {
-    data <- assertr::insist_rows(
+    data <- insist_rows(
       data = data,
       row_reduction_fn = row_reduction_fn,
       predicate_generator = predicate,
@@ -61,12 +61,12 @@ assert_rows <- function(data, row_reduction_fn, predicate, ..., predicate_calcul
       skip_chain_opts = FALSE,
       obligatory = obligatory,
       description = format_description(description),
-      success_fun = assertr::success_append,
-      error_fun = assertr::error_append,
-      defect_fun = assertr::defect_append
+      success_fun = success_append,
+      error_fun = error_append,
+      defect_fun = defect_append
     )
   } else {
-    data <- assertr::assert_rows(
+    data <- assert_rows(
       data = data,
       row_reduction_fn = row_reduction_fn,
       predicate = predicate,
@@ -74,9 +74,9 @@ assert_rows <- function(data, row_reduction_fn, predicate, ..., predicate_calcul
       skip_chain_opts = FALSE,
       obligatory = obligatory,
       description = format_description(description),
-      success_fun = assertr::success_append,
-      error_fun = assertr::error_append,
-      defect_fun = assertr::defect_append
+      success_fun = success_append,
+      error_fun = error_append,
+      defect_fun = defect_append
     )
   }
 }
